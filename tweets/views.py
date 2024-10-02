0ddeae82bb48956c9205329e21e04413631fb2d7
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2024-10-02 10:38:22 +0900

    Airbnb Challenge - Assignment 27 - Urls, Views and Templates

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Tweet
+
 
 # Create your views here.
+def tweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "all_tweets.html",
+        {
+            "tweets": tweets,
+            "title": "Tweets",
+        },
+    )
+
+
+def tweet(request, pk):
+    try:
+        tweet = Tweet.objects.get(pk=pk)
+        return render(
+            request,
+            "one_tweet.html",
+            {
+                "tweet": tweet,
+            },
+        )
+    except Tweet.DoesNotExist:
+        return render(
+            request,
+            "one_tweet.html",
+            {
+                "not_found": True,
+            },
+        )
